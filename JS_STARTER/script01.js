a12d36825f735746b64a454be859711e6d4313b9
Lasitha Ransara <lasitharansara87@gmail.com>
2023-06-09 23:58:56 +0530

    script.js
    Learning JS

--- JS_STARTER/script01.js
+++ JS_STARTER/script01.js
@@ -1,1 +1,39 @@
-console.log('Hello from seperated file!');
+/*var name = 'Alex';
+console.log(name);
+
+var schoolTeam = 'basketball';
+var schoolNumber= 20;
+
+var isRegistered = true;
+console.log('isRegistered');
+console.log(schoolNumber);
+
+var job;
+console.log(job);
+
+job = 'student';
+console.log(job);
+
+console.log(name +' '+ schoolNumber);
+
+alert(schoolTeam); */
+
+//BASIC OPERATORS//
+
+/*var distanceAlex= 20-12;
+console.log(distanceAlex);*/
+
+//LOGICAL OPERATORS//
+
+/*var distanceToSchool, distanceToHome;
+
+distanceToHome = 14;
+distanceToSchool = 6;
+
+var isCloser= distanceToHome < distanceToSchool;
+console.log(isCloser); */
+
+//BOOLEAN LOGIC//
+
+
+
