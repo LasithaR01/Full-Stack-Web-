9b620a8d73189b732c137806f7bb3ecec196da8f
Lasitha Ransara <lasitharansara87@gmail.com>
2023-06-10 23:11:00 +0530

    Update script01.js
    Learn JS Basic

--- JS_STARTER/script01.js
+++ JS_STARTER/script01.js
@@ -33,7 +33,23 @@ distanceToSchool = 6;
 var isCloser= distanceToHome < distanceToSchool;
 console.log(isCloser); */
 
-//BOOLEAN LOGIC//
+var gradeAlex = (80 + 90 + 95) /3;
+var gradeSara = (89 + 76 + 98) /3;
+var gradeNancy = (42 + 98 + 83) /3;
+
+console.log('Alex avarage grade', gradeAlex);
+console.log('Sara avarage grade', gradeSara);
+console.log('Nancy avarage grade', gradeNancy);
+
+if (gradeAlex > gradeSara && gradeAlex > gradeNancy) {
+    console.log('Alex has the highest grade',gradeAlex);
+} else if (gradeSara > gradeAlex && gradeSara > gradeNancy) {
+    console.log('Sara has the highest grade',gradeSara);
+} else if (gradeNancy > gradeAlex && gradeNancy gradeSara) {
+    console.log('Nancy has the highest grade',gradeNancy);
+} else {
+    console.log('Maybe there is same Average')
+}
 
 
 
