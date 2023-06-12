92c25f6e83a354ecda00f74507a2007d1e9c7def
Lasitha Ransara <lasitharansara87@gmail.com>
2023-06-12 19:04:12 +0530

    Update script01.js
    Learn Loops

--- JS_STARTER/script01.js
+++ JS_STARTER/script01.js
@@ -33,23 +33,82 @@ distanceToSchool = 6;
 var isCloser= distanceToHome < distanceToSchool;
 console.log(isCloser); */
 
-var gradeAlex = (80 + 90 + 95) /3;
+/*var gradeAlex = (80 + 90 + 95) /3;
 var gradeSara = (89 + 76 + 98) /3;
 var gradeNancy = (42 + 98 + 83) /3;
 
 console.log('Alex avarage grade', gradeAlex);
 console.log('Sara avarage grade', gradeSara);
-console.log('Nancy avarage grade', gradeNancy);
+console.log('Nancy avarage grade', gradeNancy);*/
 
-if (gradeAlex > gradeSara && gradeAlex > gradeNancy) {
+/*if (gradeAlex > gradeSara && gradeAlex > gradeNancy) {
     console.log('Alex has the highest grade',gradeAlex);
-} else if (gradeSara > gradeAlex && gradeSara > gradeNancy) {
+}
+ else if (gradeSara > gradeAlex && gradeSara > gradeNancy) {
     console.log('Sara has the highest grade',gradeSara);
-} else if (gradeNancy > gradeAlex && gradeNancy gradeSara) {
+} 
+else if (gradeNancy > gradeAlex && gradeNancy gradeSara) {
     console.log('Nancy has the highest grade',gradeNancy);
-} else {
+} 
+else if {
     console.log('Maybe there is same Average')
+}*/
+
+///WHILE LOOPS///
+
+/*var number = 1;
+
+while (number < 11){
+console.log(number);
+number++; //number = number + 1
+}*/
+
+/*var number = 132;
+
+while(number < 149){
+    console.log(number);
+    number +=2;
 }
 
+console.log('Number Divisible By 7');
 
+var count = 25;
 
+while (count <= 100) {
+    if (count % 7 === 0){
+      console.log(count);
+    }
+
+    count++;
+} */
+
+/*for (var i = 132; i <= 148; i++) {
+    if (i % 2 !== 0){
+        console.log(i);
+    }
+}
+
+for (var number = 1; number < 11; number++) {
+    console.log(number);
+}*/
+for (var i = 25; i <= 100; i++) {
+    if ((i % 7 === 0) && (i % 5 === 0)) {
+        console.log(i);
+    }
+}
+ 
+console.log('continue statement');
+
+for (var i = 1; i<= 10; i++) {
+    if (i % 2 !== 0) continue;
+    console.log(i);
+}
+
+console.log('break statement');
+
+for (var i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+        break;
+    }
+}
