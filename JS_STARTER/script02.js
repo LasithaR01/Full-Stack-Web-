4bcb79a6e4a98746111b7db9b92c4256b92f9c49
Lasitha Ransara <lasitharansara87@gmail.com>
2023-06-23 22:51:02 +0530

    script.js,indexo1.html
    Learn Event Handlers

--- JS_STARTER/script02.js
+++ JS_STARTER/script02.js
@@ -22,5 +22,56 @@ console.log(selectWithClass);*/
 
 ///MANIPULATION METHODS///
 
+///EVENT HANDLERS///
 
+/*var button = document.querySelector('#btn1');
+var title = document.querySelector('#OAKTitle');
 
+button.addEventListener('click', function () {
+    console.log('You clicked the button');
+    title.style.backgroundColor = 'red'
+});
+
+var listParent = document.querySelector('ul');
+    listParent.addEventListener('click', function () {
+    console.log('You clicked the ul');
+    listParent.style.backgroundColor = 'blue';
+})
+
+///QUIZ SOLUTION///
+var listItems = document.querySelectorAll ('li');
+console.log('listItems',listItems);
+
+for (var i = 0; i < listItems.length; i++) {
+    listItems[i].addEventListener('click', function (){
+        this.style.color = 'white';
+    })
+}
+
+var oakParagraph = document.querySelector('#oakParagraph');
+
+function toggleBackground() {
+    oakParagraph.classList.toggle('toggleBackground');
+    }
+
+oakParagraph.addEventListener('click', toggleBackground);
+
+oakParagraph.addEventListener('click', toggleBackground ());*/
+
+var todoListItems = document.querySelectorAll('.todoItem');
+
+for (var i = 0; i < todoListItems.length; i++) {
+  todoListItems[i].addEventListener('mouseover', function () {
+    this.style.backgroundColor = 'blue'
+    this.style.color = 'white'
+  })
+
+  todoListItems[i].addEventListener('mouseout', function () {
+    this.style.backgroundColor = 'white'
+    this.style.color = 'black'
+  })
+
+  todoListItems[i].addEventListener('click', function () {
+    this.classList.add('okay');
+  })
+}
